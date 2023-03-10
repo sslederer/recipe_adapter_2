ebd15dbf0769ce9050674ef12709bd8ef2203734
sslederer <samuellederer@gmail.com>
2023-03-09 16:23:24 -0800

    Added about tab

--- website_tabs.py
+++ website_tabs.py
@@ -3,7 +3,6 @@ from recipe_scrapers import scrape_me
 import parsing
 import dill
 import tfidf
-from parse_website import get_metadata
 import time
 
 #title
@@ -23,7 +22,6 @@ with tab1:
     target_recipe = {'label': title, 'ingredients': ingredients,'url': text}
 
     # display original recipe
-    metadata = get_metadata(text)
     st.write('#### Your recipe: ',target_recipe['label'])
 
     # get category and relevant data
@@ -65,11 +63,11 @@ with tab1:
         scores, matches, indices = tfidf.get_best(target_recipe, recipes, \
                             restrictions=restrictions,number_results=2*number)
         ingredients = set(target_recipe['ingredients']) 
-        print('finding matches took', time.time()-start)
+        # print('finding matches took', time.time()-start)
         
         match_count = 0
         while matches and match_count < number:
-            print('match count', match_count)
+            # print('match count', match_count)
             match = matches.pop(0)
             index = indices.pop(0)
             #Known problem recipe that often shows up
@@ -88,6 +86,47 @@ with tab1:
             st.write('### No suitable substitutes in database')
 
 with tab2:
-    st.header("Data statistics and standardization")
-    st.write('### Test lorem ipsum Test lorem ipsumTest lorem ipsumTest \
-             lorem ipsumTest lorem ipsumTest lorem ipsumTest lorem ipsum')
+    st.header('Data standardization')
+    
+    st.markdown("The recipe database from which the recommendations are drawn comes from the\
+        [edamam.com](https://edamam.com) API, which supplies a wealth of data for each recipe. Within the recipe corpuses, \
+        which consist of approximately 10,000 recipes for pastas and 22,000 for soups and stews, \
+        ingredients vary dramatically in their frequency. As shown in the left panels of Figures \
+        1 and 2, the numbers of appearances range from 1 to more than 1,000. The TF-IDF processing  \
+        of the recipes helps to tame this dynamic range, but additional preprocessing prior  \
+        to applying the TF-IDF map was found to improve the quality of recommendations\
+        ")
+    
+    st.markdown('This preprocessing serves to standardize the form of ingredients in the recipes, \
+                and is implemented in the function parsing.py in the  \
+                [github repository](https://github.com/sslederer/recipe_adapter_2/).\
+                The first stage isolates ingredient names from their quantities, for instance  \
+                replacing "3 garlic cloves" with "garlic". This is accomplished using the\
+                ingredient_parser package, built on nltk. The next step is more discretionary,\
+                making use of domain knowledge to dramatically condense the number of ingredients\
+                in the corpus. For instance, we map "garlic powder" to "garlic," "chicken breast" \
+                to "chicken," and remove all instances of salt and pepper due to their ubiquity.\
+                The result reduces the number of ingredients in the corpus by a factor \
+                of approximately 3, and also flattens the distribution , as shown in the right panels\
+                of Figures 1 and 2.\
+                ')
+    st.image('./plots/hist_pastas.png', caption='Fig. 1: ingredient frequency distribution\
+        for the pasta corpus. The right panel shows the result after standardization.')
+    st.image('./plots/hist_soups.png', caption='Fig. 2: ingredient frequency distribution\
+        for the soup and stew corpus. The right panel shows the result after standardization.')
+    
+    st.header('Recommendation performance')
+    st.markdown("It would be desirable to evaluate the performance of the app\
+            in some objective fashion. Necessarily, this would require some \
+            form of human input, which is beyond the scope of the present effort. \
+            Nevertheless, a means of numerical evaluation is available by comparing \
+            the similarity scores (cosine similarities) of the top recommendation(s)\
+            to the overall distribution of scores. We have not carried out a formal analysis\
+            along these lines, but Figure 3 illustrates the idea. The distributions\
+            are strongly peaked near zero, with the largest values much greater than the mean. \
+            According to this crude metric, the top recommendations are many times better\
+            than choosing a recipe at random, as expected.\
+            ")
+    st.image('./plots/scores.png', caption='Fig. 3: distribution of similarity scores \
+        for selected recipes. The vertical orange lines show the expected \
+        value of the similarity score over the distribution.')
